407a8b59e75745beb8ba4de811ce54bedf119416
realdennis <realdennis777@gmail.com>
2021-06-05 08:12:00 +0800

    fix: postinstall script

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,8 @@
-import ipfs from "./ipfs.js";
-import debug from "./debug.js";
+import ipfs from './ipfs.js';
+import debug from './debug.js';
+import repo from './repo.js';
 export default {
   debug,
   ipfs,
+  repo,
 };
