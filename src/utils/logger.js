39c7e3b07c91e701e70514acb7914fbe6c41dfc6
realdennis <realdennis777@gmail.com>
2021-06-05 17:23:24 +0800

    fix: logger display issue

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,6 +1,6 @@
-import isDev from "./isDev.js";
+import isDev from './isDev.js';
 const logger = isDev
-  ? (message) => console.log(`[DEBUG]: ${message}`)
+  ? (...message) => console.info('[DEBUG]: ', ...message)
   : () => {};
 
 export default logger;
