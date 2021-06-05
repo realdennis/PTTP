9a69983489db679a335d1efe45c7ea19c97ea45a
realdennis <realdennis777@gmail.com>
2021-06-05 08:15:13 +0800

    fix(argv): correct the nickname data

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 #!/usr/bin/env node
-import React from "react";
-import App from "./app.js";
-import { render } from "ink";
+import React from 'react';
+import App from './app.js';
+import { render } from 'ink';
 
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
-import getNode from "./lib/getNode.js";
-import crypto from "crypto";
+import getNode from './lib/getNode.js';
+import crypto from 'crypto';
 
-import config from "./config/index.js";
+import config from './config/index.js';
 
-import logger from "./utils/logger.js";
-import isDev from "./utils/isDev.js";
+import logger from './utils/logger.js';
+import isDev from './utils/isDev.js';
 
 const main = async () => {
   const node = await getNode();
 
-  const { nickname } = yargs(hideBin(process.argv)).option("nickname", {
-    type: "string",
-    description: "nickname, default is anomymous",
-    default: "anomymous",
-  });
+  const { nickname } = yargs(hideBin(process.argv)).option('nickname', {
+    type: 'string',
+    description: 'nickname, default is anomymous',
+    default: 'anomymous',
+  }).argv;
   const createHandler = () => {
     const topicID = isDev ? config.debug.topic : crypto.randomUUID();
     render(
       <App
-        mode={"create"}
+        mode={'create'}
         nickname={nickname}
         ipfsNode={node}
         topicID={topicID}
@@ -36,10 +36,10 @@ const main = async () => {
 
   const joinHandler = ({ room }) => {
     const topicID = isDev ? config.debug.topic : room;
-    logger("join handler room=", topicID);
+    logger('join handler room=', topicID);
     render(
       <App
-        mode={"join"}
+        mode={'join'}
         nickname={nickname}
         ipfsNode={node}
         topicID={topicID}
@@ -47,21 +47,21 @@ const main = async () => {
     );
   };
   yargs(hideBin(process.argv))
-    .scriptName("ptp")
-    .command("create", "create the unique room!", () => {}, createHandler)
+    .scriptName('ptp')
+    .command('create', 'create the unique room!', () => {}, createHandler)
     .command(
-      "join [room]",
-      "join the unique room!",
+      'join [room]',
+      'join the unique room!',
       (yargs) => {
-        yargs.positional("room", {
-          type: "string",
-          default: "",
-          describe: "join [uuid]",
+        yargs.positional('room', {
+          type: 'string',
+          default: '',
+          describe: 'join [uuid]',
         });
       },
       joinHandler
     )
-    .demandCommand(1, "You need at least one command.")
+    .demandCommand(1, 'You need at least one command.')
     .help().argv;
 };
 main();
