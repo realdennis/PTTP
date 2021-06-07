b03a20ccc60ac7bf5eb697a81a3fa3fae26eb885
realdennis <realdennis777@gmail.com>
2021-06-07 19:52:02 +0800

    feat(text-input): integrate the text input

--- src/ui/app.js
+++ src/ui/app.js
@@ -1,59 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { Text, Box, useInput } from 'ink';
+import React, { useState } from 'react';
+import { Box, Text } from 'ink';
 import TextInput from 'ink-text-input';
 
-const useIpfsSubMessage = ({
-  node,
-  nickname,
-  topic,
-  authPeerID,
-  sessionKey,
-}) => {
-  const [messages, setMessages] = useState([]);
-  const callback = (mes) => {
-    const { data: payload, from } = mes;
-    const { cipherText, nickname } = payload;
-    if (from !== authPeerID) {
-      return;
-    }
-    setMessages((messages) => [
-      ...messages,
-      {
-        text: cipherText,
-        nickname,
-        key: cipherText + Date.now(),
-      },
-    ]);
-  };
-  useEffect(() => {
-    node.pubsub.subscribe(topic, callback);
-    return () => node.pubsub.unsubscribe(topic, callback);
-  }, []);
-
-  const sendMessage = (text) =>
-    node.pubsub.publish(topic, {
-      cipherText: text,
-      nickname,
-    });
-  return [messages, sendMessage];
-};
-
-const App = ({ nickname, node, sessionKey, authPeerID, topic }) => {
-  const [messages, sendMessage] = useIpfsSubMessage({
-    node,
-    nickname,
-    topic,
-    authPeerID,
-    sessionKey,
-  });
-
-  const [input, setInput] = useState('');
-
+const App = () => {
+  const [query, setQuery] = useState('');
   return (
-    <>
-      <Text color="red">Nickname: {nickname}</Text>
-      <Text color="red">Roomname: {topic}</Text>
-    </>
+    <Box>
+      <Box marginRight={1}>
+        <Text>Enter your query:</Text>
+      </Box>
+
+      <TextInput
+        value={query}
+        onChange={setQuery}
+        onSubmit={() => {
+          setQuery('');
+        }}
+      />
+    </Box>
   );
 };
 export default App;
