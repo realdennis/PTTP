758db2d442d04259279e342dd446e363bf18939f
realdennis <realdennis777@gmail.com>
2021-06-12 22:47:33 +0800

    chore: update the ipfs config

---
+++ src/config/Swarm.js
@@ -0,0 +1,2 @@
+const Swarm = ['/ip4/0.0.0.0/tcp/0', '/ip4/127.0.0.1/tcp/0/ws'];
+export default Swarm;
--- src/config/ipfs.js
+++ src/config/ipfs.js
@@ -1,9 +1,17 @@
 import path from 'path';
 import os from 'os';
 import repo from './repo.js';
+import Bootstrap from './Bootstrap';
+import Swarm from './Swarm';
 import isDev from '../utils/isDev.js';
 
 export default {
   silent: !isDev,
   repo: path.resolve(os.homedir(), repo.folder),
+  Bootstrap,
+  config: {
+    Addresses: {
+      Swarm,
+    },
+  },
 };
