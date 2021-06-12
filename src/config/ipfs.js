155afa7a7b30fedd2c42d2a71da52a37ff09c956
realdennis <realdennis777@gmail.com>
2021-06-12 20:04:31 +0800

    feat: relay and hop

--- src/config/ipfs.js
+++ src/config/ipfs.js
@@ -1,9 +1,11 @@
 import path from 'path';
 import os from 'os';
 import repo from './repo.js';
+import relay from './relay';
 import isDev from '../utils/isDev.js';
 
 export default {
   silent: !isDev,
   repo: path.resolve(os.homedir(), repo.folder),
+  relay,
 };
