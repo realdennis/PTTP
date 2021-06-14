55963b2bb3acc615ab6c97d3ef5502f5b403659e
realdennis <realdennis777@gmail.com>
2021-06-15 00:50:21 +0800

    feat(wrtc): integrate with wrtc

--- src/config/Swarm.js
+++ src/config/Swarm.js
@@ -1,2 +1,7 @@
-const Swarm = ['/ip4/0.0.0.0/tcp/0', '/ip4/127.0.0.1/tcp/0/ws'];
+const Swarm = [
+  '/ip4/0.0.0.0/tcp/0',
+  '/ip4/127.0.0.1/tcp/0/ws',
+  '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
+  '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
+];
 export default Swarm;
--- src/config/ipfs.js
+++ src/config/ipfs.js
@@ -3,6 +3,8 @@ import os from 'os';
 import repo from './repo.js';
 import Swarm from './Swarm';
 import isDev from '../utils/isDev.js';
+import wrtc from 'wrtc';
+import WebRTCStar from 'libp2p-webrtc-star';
 
 export default {
   silent: !isDev,
@@ -10,6 +12,25 @@ export default {
   config: {
     Addresses: {
       Swarm,
+    },
+  },
+  libp2p: {
+    modules: {
+      transport: [WebRTCStar],
+    },
+    config: {
+      peerDiscovery: {
+        webRTCStar: {
+          // <- note the lower-case w - see https://github.com/libp2p/js-libp2p/issues/576
+          enabled: true,
+        },
+      },
+      transport: {
+        WebRTCStar: {
+          // <- note the upper-case w- see https://github.com/libp2p/js-libp2p/issues/576
+          wrtc,
+        },
+      },
     },
   },
 };
